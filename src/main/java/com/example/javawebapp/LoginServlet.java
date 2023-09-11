c73ab1d13772e6a544b9c35fbcaebd2d02032298
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2023-09-10 23:09:39 -0300

    Arrumei algumas coisas

--- src/main/java/com/example/javawebapp/LoginServlet.java
+++ src/main/java/com/example/javawebapp/LoginServlet.java
@@ -1,14 +1,26 @@
 package com.example.javawebapp;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "login", value = "/login")
 public class LoginServlet extends HttpServlet {   
 
-    //VARÁVEIS
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String senha = req.getParameter("senha");
+        // salvar no banco de dados
+        // enviar um email para o admin com a mensagem
+      
+        System.out.println(email);
+        System.out.println(senha);
 
-    // 4. sobreescrever os métodos do???  doGet, doPost, doDelete, etc
-    // e definir o comportamento
-    
+    }
+
 }
