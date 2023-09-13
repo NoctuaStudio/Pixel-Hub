d04e6f4feac1de8d6a2780e57abb504cd6c2e5a8
Caetano Costa <caetanogonca@gmail.com>
2023-09-12 22:34:10 -0300

    SuporteServlet feito

--- src/main/java/com/example/javawebapp/SuporteServlet.java
+++ src/main/java/com/example/javawebapp/SuporteServlet.java
@@ -1,5 +1,29 @@
 package com.example.javawebapp;
 
-public class SuporteServlet {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+// 1. criar uma classe em java
+// 2. extends HttpServlet
+// 3. "roteamento" anotar a classe com @WebServlet definir
+// name e value
+// 4. sobreescrever os métodos do???  doGet, doPost, doDelete, etc
+// e definir o comportamento
+
+@WebServlet(name = "suporte", value = "/suporte")
+public class SuporteServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String mensagem = req.getParameter("suporte_mensagem");       
+       
+        System.out.println(mensagem);
+    }
     
 }
