2945283e4cfee18b8be316fce103d50ce31e6592
Gustavo Lemos <galaxywander0@gmail.com>
2023-11-21 15:23:57 -0300

    cadastrarProduto validators!

--- src/main/java/com/example/javawebapp/ProdutoCadastroServlet.java
+++ src/main/java/com/example/javawebapp/ProdutoCadastroServlet.java
@@ -1,12 +1,19 @@
 package com.example.javawebapp;
 
 import java.io.IOException;
+import java.util.Set;
+
+import com.example.javawebapp.forms.CadastroForm;
+import com.example.javawebapp.forms.ProdutoCadastroForm;
+import com.example.javawebapp.usuario.UsuarioDao;
+import com.example.javawebapp.validators.ValidatorUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
 
 // 1. criar uma classe em java
 // 2. extends HttpServlet
@@ -25,8 +32,38 @@ public class ProdutoCadastroServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String nome = req.getParameter("nome");
+        String descricao = req.getParameter("descricao");
+        String preco = req.getParameter("preco");
+        String quantidade = req.getParameter("quantidade");
+        String categoria = req.getParameter("categoria");
+        String imagem = req.getParameter("imagem");
 
+        ProdutoCadastroForm produtoCadastroForm = new ProdutoCadastroForm(nome, descricao, preco, quantidade, categoria, imagem);
+
+        Set<ConstraintViolation<ProdutoCadastroForm>> violations = ValidatorUtil.validateObject(produtoCadastroForm);
+    
+        if (violations.isEmpty()) {
+            if(nome == descricao){
+               // req.setAttribute("erroEmail", "Já existe um usuário com esse email!");
+                //req.getRequestDispatcher("WEB-INF/cadastro.jsp").forward(req, res);
+            } else{
+                // AQUI DataBase
+                //UsuarioDao.cadastrar(nome, sobrenome, username, senha, email, telefone, endereco, cep, Integer.parseInt(cidade));
+                //res.sendRedirect("index.jsp");
+            }
+        } else {
+            req.setAttribute("nome", nome);
+            req.setAttribute("descricao", descricao);
+            req.setAttribute("preco", preco);
+            req.setAttribute("quantidade", quantidade);
+            req.setAttribute("categoria", categoria);
+            req.setAttribute("imagem", imagem);
+            req.setAttribute("violations", violations);
+            req.getRequestDispatcher("WEB-INF/cadastrar-produtos.jsp").forward(req, res);
+        }
     }
-
 }
+
+
 
--- src/main/java/com/example/javawebapp/forms/ProdutoCadastroForm.java
+++ src/main/java/com/example/javawebapp/forms/ProdutoCadastroForm.java
@@ -1,5 +1,89 @@
 package com.example.javawebapp.forms;
+import java.util.ArrayList;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 public class ProdutoCadastroForm {
-    
+    @NotNull
+    @NotBlank
+    private String nome;
+
+    @NotNull
+    @NotBlank
+    private String descricao;
+
+    @NotNull
+    @NotBlank
+    private String preco;
+
+    @NotNull
+    @NotBlank
+    private String quantidade;
+
+    @NotNull
+    @NotBlank
+    private String categoria;
+
+    @NotNull
+    @NotBlank
+    private String imagem;
+
+    public ProdutoCadastroForm(String nome, String descricao, String preco, String quantidade, String categoria, String imagem) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+
 }
